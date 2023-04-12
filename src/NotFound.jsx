485a88b1a895566f300ea7230f90c3587751c6fe
sayembinamir] <sayembinamir912@gmail.com>
2023-04-12 11:02:27 +0600

    some prblem

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>404 not found </h1>
+            <h2>404 not found </h2>
         </div>
     );
 };
